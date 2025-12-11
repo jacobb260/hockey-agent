f83c66e381bfbc608aae206be7b93447a69c190c
jacobb260 <jacob.lindstrombjareklint@gmail.com>
2025-12-11 17:49:25 +0100

    Some initial stuff

---
+++ config.py
@@ -0,0 +1,51 @@
+from dotenv import load_dotenv
+import os
+
+
+# Ladda .env automatiskt när modulen importeras
+load_dotenv()
+
+
+class Settings:
+    """
+    Samlad konfiguration för projektet.
+    Läser värden från miljövariabler (.env).
+    """
+
+    def __init__(self) -> None:
+        #Kommentera bort när vi har key till Gemini
+        """
+        # LLM / Gemini
+        self.GOOGLE_API_KEY = self._get_env("GOOGLE_API_KEY")
+        self.GEMINI_MODEL_NAME = self._get_env("GEMINI_MODEL_NAME", "gemini-1.5-pro")
+        """
+        # Hopsworks
+        self.HOPSWORKS_API_KEY = self._get_env("HOPSWORKS_API_KEY")
+        self.HOPSWORKS_PROJECT = self._get_env("HOPSWORKS_PROJECT")
+        self.HOPSWORKS_HOST = self._get_env(
+            "HOPSWORKS_HOST",
+            "eu-west.cloud.hopsworks.ai",
+        )
+
+        # NHL API
+        self.NHL_API_BASE_URL = self._get_env(
+            "NHL_API_BASE_URL",
+            "https://statsapi.web.nhl.com/api/v1",
+        )
+        
+
+    @staticmethod
+    def _get_env(name: str, default: str | None = None) -> str:
+        """
+        Hämtar en miljövariabel.
+        - Om default är None och variabeln saknas → raise fel
+        - Om default finns → använd default om variabeln saknas
+        """
+        value = os.getenv(name, default)
+        if value is None:
+            raise RuntimeError(f"Missing required environment variable: {name}")
+        return value
+
+
+# Global instans som du kan importera överallt
+settings = Settings()
