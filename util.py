2ab230c46dd987bbef88b28ff0707aa2871bbd2b
jacobb260 <jacob.lindstrombjareklint@gmail.com>
2025-12-21 12:49:03 +0100

    Fixed so that all player stats from each games gets fetched and some code cleaning
    The nhl api has a max limit of 10 000 datapoints for each fetch.

--- util.py
+++ util.py
@@ -1,8 +1,18 @@
-import datetime
 import requests
 import pandas as pd
 import re
-import time
+from datetime import datetime, date, timedelta
+import calendar
+
+def generate_season_ids(start_year=2000):
+    current_year = datetime.now().year
+
+    season_ids = []
+    for year in range(start_year, current_year+1):
+        season_ids.append(f"{year}{year+1}")
+
+    return season_ids
+
 
 def get_season(date):
     year = date.year
@@ -57,27 +67,47 @@ def to_snake(name: str) -> str:
 def fetch_player_form_for_season(season_id: str) -> pd.DataFrame:
     PLAYER_GAME_LOG_URL = "https://api.nhle.com/stats/rest/en/skater/summary"
 
-    params = {
-        "isGame": "true",  
-        "cayenneExp": f"gameTypeId=2 and seasonId={season_id}",
-        "limit": -1  
-    }
+    start_year = int(season_id[:4]) 
+    end_year = int(season_id[4:]) 
+
+    season_start = date(start_year, 10, 1)
+    season_end = date(end_year, 4, 30)
+    all_months = []
+    #Fetch per month in order to get all player stats. The max limit from the api is 10 000 datapoints
+    current = season_start 
+    while current <= season_end:
+        last_day_of_month = calendar.monthrange(current.year, current.month)[1]
+        month_end = date(current.year, current.month, last_day_of_month)
+
+        range_end = min(month_end, season_end)
+
+        params = {
+            "isGame": "true",  
+            "cayenneExp": (
+            f"gameTypeId=2 and seasonId={season_id} "
+            f"and gameDate>='{current.isoformat()}' and gameDate<='{range_end.isoformat()}'"),
+            "limit": -1  
+        }
+        
+        print(f"Downloading {current} to {range_end} for season {season_id}")
+        resp = requests.get(PLAYER_GAME_LOG_URL, params=params, timeout=20)
+        resp.raise_for_status()
+        
+        data = resp.json().get("data")
+
+        if data:
+            df_month = pd.DataFrame(data)
+            print("Shape: " + str(df_month.shape))
+            if "seasonId" not in df_month.columns:
+                df_month["seasonId"] = int(season_id)
+            all_months.append(df_month)
+        
+        current = range_end + timedelta(days=1)
     
-    resp = requests.get(PLAYER_GAME_LOG_URL, params=params, timeout=20)
-    resp.raise_for_status()
-    
-    data = resp.json()
-    
-    if "data" not in data or not data["data"]:
+    if not all_months:
         return pd.DataFrame()
     
-    df = pd.DataFrame(data["data"])
-    
-    # Säkerställ att seasonId finns
-    if "seasonId" not in df.columns:
-        df["seasonId"] = season_id
-    
-    return df
+    return pd.concat(all_months, ignore_index=True)
 
 # URL för målvakter
 GOALIE_GAME_LOG_URL = "https://api.nhle.com/stats/rest/en/goalie/summary"
