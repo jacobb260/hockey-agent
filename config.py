4182e33de225d628a0721bf1bf60c897a07f5c51
jacobb260 <jacob.lindstrombjareklint@gmail.com>
2026-01-02 12:14:40 +0100

    Code cleaning

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Settings:
        
         # LLM / Gemini
         self.GOOGLE_API_KEY = self._get_env("GOOGLE_API_KEY", "none",)
-        #self.GEMINI_MODEL_NAME = self._get_env("GEMINI_MODEL_NAME", "gemini-1.5-pro")
+        
         # Hopsworks
         self.HOPSWORKS_API_KEY = self._get_env("HOPSWORKS_API_KEY")
         self.HOPSWORKS_PROJECT = self._get_env("HOPSWORKS_PROJECT")
@@ -24,20 +24,7 @@ class Settings:
             "HOPSWORKS_HOST",
             "eu-west.cloud.hopsworks.ai",
         )
-
-        # NHL API
-        self.NHL_STATS_BASE_URL = self._get_env(
-            "NHL_STATS_BASE_URL",
-            "https://api.nhle.com/stats/rest",
-        )
-
-        self.NHL_WEB_BASE_URL = self._get_env(
-            "NHL_WEB_BASE_URL",
-            "https://api-web.nhle.com",
-        )
-
-        
-        
+         
 
     @staticmethod
     def _get_env(name: str, default: str | None = None) -> str:
